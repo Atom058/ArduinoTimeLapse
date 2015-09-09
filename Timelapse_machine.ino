87e9d09d95c0c7389f4667fafbe491860c5f98a3
atom058 <anton.dahlstrom508@gmail.com>
2015-09-10 01:04:35 +0200

    Some resetting of test code

--- Timelapse_machine.ino
+++ Timelapse_machine.ino
@@ -17,32 +17,27 @@ const byte d6 = 6;
 const byte d7 = 7;
 
 //Pins for motor
-const byte motor_pin1 = 8;
-const byte motor_pin2 = 9;
-const byte motor_pin3 = 12;
-const byte motor_pin4 = 13;
+const byte motorPin1 = 8;
+const byte motorPin2 = 9;
+const byte motorPin3 = 12;
+const byte motorPin4 = 13;
 
 // Pins that aren't connected to the library, but are still related to LCD
-const byte backlight = 10;
 const byte contrast = 11;
 
-//Other Pins - Probably not bytes?
-const byte button_up = A1;
-const byte button_down = A2;
-const byte button_menu = A3;
+//Other Pins
+const byte buttonUp = A1;
+const byte buttonDown = A2;
+const byte buttonMenu = A3;
 //Reset button - should be connected to a pnp to LCD/Motor and to Arduino
 
 //Variables
-float current_degree = 0;
-int backlight_level = 128;
-boolean UP = true;
-boolean DOWN = false;
-boolean oscillation_direction = UP;
+float currentMotorPosition = 0;
 String output = "";
 
 // The stepper motor is geared, so it has 64 internal steps per
 //   revolution, but that translates 64x32 steps on the engine.
-Stepper myStepper(stepsPerRevolution, motor_pin1, motor_pin2, motor_pin3, motor_pin4);
+Stepper myStepper(stepsPerRevolution, motorPin1, motorPin2, motorPin3, motorPin4);
 
 LiquidCrystal lcd(rs, enable, d4, d5, d6, d7);
 
@@ -53,38 +48,42 @@ void setup() {
   //LCD
   lcd.begin(16, 2);
   lcd.noAutoscroll();
-  pinMode(backlight, OUTPUT);
-  analogWrite(backlight, backlight_level);
+
 }
 
+unsigned long loopTime = 0;
+bool timelapseRunning = false;
+
 void loop() {
+
+  if( intialContrastSet ){
+    loopTime = millis();
+
+    readButtons();
+    updateDisplay();
+
+    if( timelapseRunning ) {
+      turnMotor();
+    }
+  } else {
+    readButtons();
+    updateDisplay();
+  }
   
+  /*
   lcd.setCursor(0, 0);
   lcd.print("Total degrees:");
   lcd.setCursor(0,1);
-  output = String(current_degree);
+  output = String(currentMotorPosition);
   lcd.print(output);
   myStepper.step(oneDegree);
   delay(500);
-  current_degree += oneDegree * stepResolution;
+  currentMotorPosition += oneDegree * stepResolution;
+  */
   
-
-  byte stepping_amount = 25;
-  //For Backlighting correction
-  if( oscillation_direction == UP ) {
-    backlight_level += stepping_amount;
-    if( backlight_level > 255 ){
-      backlight_level = 255;
-      oscillation_direction = DOWN;
-    }
-  } else {
-    backlight_level -= stepping_amount;
-    if( backlight_level < 0 ){
-      backlight_level = 0;
-      oscillation_direction = UP;
-    }
-  }
-  
-  analogWrite(backlight, backlight_level); 
 }
 
+unsigned long startButtonPress = 0;
+void readButtons() {}
+
+void updateDisplay() {}
